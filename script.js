907d6df0ac392d9e550ebdf23f50a08f064867d6
Yannick Liebnau <yannickliebnau@MacBook-Pro-de-Yannick.local>
2024-02-09 08:23:54 +0100

    quick refacto js

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-const superSwiper = () => {
-  const swiper = new Swiper('.swiper', {
-    init: false,
-    direction: "vertical",
-    slidesPerView: 1,
-    mousewheel: true,
-    effect: 'fade',
-    on: {
-      slideChange() {
-        if (swiper.isEnd) {
-          window.addEventListener('wheel', event => {
-            if (event.deltaY > 50) {
-              setTimeout(() => {
-                swiper.mousewheel.disable();
-                document.querySelector('.swiper').classList.remove('fixed');
-              }, 500)
-            }
-          })
-        }
-      }
-    }
-  })
-
-  swiper.on('init', () => {
-    document.querySelector('.swiper').classList.add('fixed');
-    if (swiper.isBeginning) {
-      window.addEventListener('wheel', event => {
-        if (event.deltaY < -50) {
-          setTimeout(() => {
-            swiper.mousewheel.disable();
-            document.querySelector('.swiper').classList.remove('fixed');
-          }, 200)
-        }
-      })
-    };
-  })
-
-  swiper.init();
-}
-
 const swiperObserver = () => {
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -49,13 +9,58 @@ const swiperObserver = () => {
   }, {
     root: null,
     threshold: 0.9
-  }
-  )
+  });
   const swiper = document.querySelector('.swiper');
   observer.observe(swiper);
 }
 
+const superSwiper = () => {
+  const swiper = new Swiper('.swiper', {
+    init: false,
+    direction: "vertical",
+    slidesPerView: 1,
+    mousewheel: true,
+    effect: 'fade',
+    on: {
+      init: () => {
+        swiperInit();
+      },
+      slideChange: () => {
+        slideChange();
+      }
+    }
+  })
+
+  const swiperInit = () => {
+    document.querySelector('.swiper').classList.add('fixed');
+    if (swiper.isBeginning) {
+      window.addEventListener('wheel', event => {
+        if (event.deltaY < -50) {
+          setTimeout(() => {
+            swiper.mousewheel.disable();
+            document.querySelector('.swiper').classList.remove('fixed');
+          }, 200)
+        }
+      })
+    };
+  }
+
+  const slideChange = () => {
+    if (swiper.isEnd) {
+      window.addEventListener('wheel', event => {
+        if (event.deltaY > 50) {
+          setTimeout(() => {
+            swiper.mousewheel.disable();
+            document.querySelector('.swiper').classList.remove('fixed');
+          }, 500)
+        }
+      })
+    }
+  }
+
+  swiper.init();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   swiperObserver();
-
 });
